5830686902d2e5bbdf26d17a8f6f823268776fba
Oona Räisänen <okraisan@protonmail.com>
2019-10-31 14:47:03 +0200

    testejä ym

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -13,11 +13,20 @@ import static org.junit.Assert.*;
 public class VarastoTest {
 
     Varasto varasto;
+    Varasto virheellinen_varasto;
+    Varasto varasto_alkusaldolla;
+    Varasto virheellinen_varasto_alkusaldolla;
+    Varasto varasto_virheellisella_alkusaldolla;
+
     double vertailuTarkkuus = 0.0001;
 
     @Before
     public void setUp() {
         varasto = new Varasto(10);
+        virheellinen_varasto = new Varasto(-1);
+        varasto_alkusaldolla = new Varasto(10, 5);
+        varasto_virheellisella_alkusaldolla = new Varasto(10, -1);
+        virheellinen_varasto_alkusaldolla = new Varasto(-1, 5);
     }
 
     @Test
@@ -28,6 +37,18 @@ public class VarastoTest {
     @Test
     public void uudellaVarastollaOikeaTilavuus() {
         assertEquals(10, varasto.getTilavuus(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void alkusaldollaOikeaSaldo() {
+        assertEquals(5, varasto_alkusaldolla.getSaldo(), vertailuTarkkuus);
+        assertEquals(0, varasto_virheellisella_alkusaldolla.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void alkusaldollaOikeaTilavuus() {
+        assertEquals(10, varasto_alkusaldolla.getTilavuus(), vertailuTarkkuus);
+        assertEquals(0, virheellinen_varasto_alkusaldolla.getTilavuus(), vertailuTarkkuus);
     }
 
     @Test
@@ -65,4 +86,32 @@ public class VarastoTest {
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
 
+    @Test
+    public void negatiivistaEiVoiOttaa() {
+        double saldo_ennen = varasto.getSaldo();
+        varasto.otaVarastosta(-2);
+        assertEquals(saldo_ennen, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void negatiivistaEiVoiLisätä() {
+        double saldo_ennen = varasto.getSaldo();
+        varasto.lisaaVarastoon(-1);
+        assertEquals(saldo_ennen, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void eiVoiOttaaLiikaa() {
+        varasto.lisaaVarastoon(20);
+        double saldo_ennen = varasto.getSaldo();
+        double saatiin = varasto.otaVarastosta(saldo_ennen + 10);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+        assertEquals(saldo_ennen, saatiin, vertailuTarkkuus);
+    }
+
+    @Test
+    public void oikeaMerkkijonoesitys() {
+        System.out.println(varasto.toString());
+        assertEquals("saldo = 0.0, vielä tilaa 10.0", varasto.toString());
+    }
 }
